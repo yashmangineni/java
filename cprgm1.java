a1e0af53f1920f0cacf7109634676f51db8caae0
[200~yashmangineni~ <yaswanthmangineni@gmail.com>
2024-07-31 12:19:53 +0530

    yash

---
+++ cprgm1.java
@@ -0,0 +1,10 @@
+class cprgm1{
+public static void main(String args[]){
+int start=1;
+while(start<=100)
+{
+System.out.println("coding hub");
+start++;
+}
+}
+}
---
+++ cprgm2.java
@@ -0,0 +1,11 @@
+class cprgm2{
+public static void main(String args[]){
+int start=1;
+while(start<=100)
+{
+System.out.println("java");
+System.out.println("programming");
+start++;
+}
+}
+}
